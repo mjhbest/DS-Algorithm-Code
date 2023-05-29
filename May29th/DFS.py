e779f87af329f209663853103cce1266eddd7e6e
Jaehyeon Myung <mjhbest@kaist.ac.kr>
2023-05-29 21:14:22 +0900

    Update DFS.py

--- May29th/DFS.py
+++ May29th/DFS.py
@@ -1,6 +1,6 @@
 graph = [
-    [],
-    [2, 3, 8],
+    [], #0
+    [2, 3, 8], #1
     [1, 7],
     [1, 4, 5],
     [3, 5],
@@ -15,12 +15,18 @@ visited = [False] * len(graph)
 
 def dfs(graph, v):
     #1. v 를 visited에 추가
-
+    visited[v] = True
     #2. v가 더 깊게 들어갈 수 있는 후보 경로 추적
-
+    for w in graph[v]:
     #3. 해당 노드가 이미 방문한적 있는지 확인
-
+        if not visited[w]:
     #4. 없다면 해당 노드아래로 dfs진행
+            dfs(graph,w)
 
 # 0번 노드가 없으니 1번 노드부터 탐색
-dfs(graph, 1)
+for i in range(9):
+    dfs(graph, i)
+
+for i in range(9):
+    if visited[i]:
+        print(i)
